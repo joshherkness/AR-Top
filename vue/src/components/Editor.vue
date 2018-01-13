6bc3805fa8ef5ec199fe90d93ba5133e78dfb409
Joshua Herkness <joshherkness@gmail.com>
2018-01-12 20:50:25 -0500

    Add `OrbitControls` to the camera responsible for rendering the map scene. This should allow the user to:
    - `click + drag` to rotate the scene
    - `mouse wheel` to zoom in and out

--- vue/src/components/Editor.vue
+++ vue/src/components/Editor.vue
@@ -1,18 +1,17 @@
 <template>
-  <div>
-    <h1>Editor</h1>
-    <div ref='canvas'></div>
-  </div>
+  <div ref='canvas' id='canvas'></div>
 </template>
 
 <script>
 import * as THREE from 'three'
+var OrbitControls = require('three-orbit-controls')(THREE)
 
 export default {
   name: 'Editor',
   data () {
     return {
       camera: null,
+      controls: null,
       scene: null,
       renderer: null,
       objects: []
@@ -20,16 +19,16 @@ export default {
   },
   mounted () {
     this.init()
+    this.onWindowResize()
     this.render()
 
     // Attach event listeners to the document
-    document.addEventListener('mousemove', this.onDocumentMouseMove, false)
-    document.addEventListener('mousedown', this.onDocumentMouseDown, false)
+    window.addEventListener('resize', this.onWindowResize, false)
   },
   methods: {
     init () {
       // Create the renderer
-      this.renderer = new THREE.WebGLRenderer({ antialias: true })
+      this.renderer = new THREE.WebGLRenderer()
       this.renderer.setPixelRatio(window.devicePixelRatio)
 
       // Create the camera
@@ -37,9 +36,15 @@ export default {
       this.camera.position.set(500, 800, 1300)
       this.camera.lookAt(new THREE.Vector3())
 
+      // Add orbit controls
+      this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+      this.controls.enablePan = false
+      this.controls.maxPolarAngle = (Math.PI / 2) + 0.1
+      this.controls.addEventListener('change', this.render)
+
       // Create the scene
       this.scene = new THREE.Scene()
-      this.scene.background = new THREE.Color(0xf0f0f0)
+      this.scene.background = new THREE.Color(0xffffff)
 
       // Add lighting
       var ambientLight = new THREE.AmbientLight(0x606060)
@@ -50,7 +55,7 @@ export default {
       this.scene.add(directionalLight)
 
       // Create the grid
-      let gridHelper = new THREE.GridHelper(1000, 20)
+      let gridHelper = new THREE.GridHelper(1000, 20, 0xafafaf, 0xafafaf)
       this.scene.add(gridHelper)
 
       // Create the plane
@@ -60,25 +65,36 @@ export default {
       this.scene.add(plane)
       this.objects.push(plane)
 
+      // Create the base
+      let base = new THREE.Mesh(new THREE.CubeGeometry(1000, 50, 1000), new THREE.MeshBasicMaterial({color: 0xf9f9f9}))
+      base.position.y = -26
+      this.scene.add(base)
+
       let container = this.$refs.canvas
       container.appendChild(this.renderer.domElement)
     },
     render () {
       this.renderer.render(this.scene, this.camera)
     },
-    onDocumentMouseMove (event) {
-      // Called when mouse moved
-    },
-    onDocumentMouseDown (event) {
-      // Called when mouse down
+    onWindowResize () {
+      this.camera.aspect = window.innerWidth / window.innerHeight
+      this.camera.updateProjectionMatrix()
+      this.renderer.setSize(window.innerWidth, window.innerHeight)
     }
   },
   destroyed () {
-
+    // Remove event listeners
+    window.removeEventListener('resize', this.onWindowResize, false)
   }
 }
 </script>
 
 <style scoped>
-
+#canvas {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
 </style>
