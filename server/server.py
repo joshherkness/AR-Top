bc6476c4a03449466f9cbe7aa8c7994e91223ad8
AnthonyHewins <aahewins@oakland.edu>
2018-01-14 16:43:19 -0500

    Added secrets.py, modified server slightly

--- server/server.py
+++ server/server.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 from flask_mongoengine import MongoEngine
 from flask_security import Security, MongoEngineUserDatastore, \
     UserMixin, RoleMixin, login_required
+from secrets import SECRET_KEY
 
 # Create app
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SECRET_KEY'] = 'super-secret'
+app.config['SECRET_KEY'] = SECRET_KEY
 
 # MongoDB Config
 app.config['MONGODB_DB'] = 'mydatabase'
@@ -39,6 +40,19 @@ def create_user():
 # Routes
 @app.route('/')
 def index(): return render_template('index.html')
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+	try:
+		if request.method == "POST":
+			email = request.form['email']
+			
+		else:
+			return {"error":""}
+	except KeyError:
+		return {"error": "Malformed request"}
+	except Exception as e:
+		return {"error": "Something went wrong"
 
 @app.route('/protected')
 @login_required
