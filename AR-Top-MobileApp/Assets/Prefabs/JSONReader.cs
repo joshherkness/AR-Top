a8bbf4556887d68a1db0ce3bd7f73cf5418badf6
goeteeks <nferman@oakland.edu>
2018-01-15 22:32:53 -0500

    Improved JSON Parser
    This time for real.

--- AR-Top-MobileApp/Assets/Prefabs/JSONReader.cs
+++ AR-Top-MobileApp/Assets/Prefabs/JSONReader.cs
@@ -21,21 +21,147 @@ public class JSONReader : MonoBehaviour {
 
 	private int gridHeightGap = 2; //The height gap for y coordinate grid spaces. Every increment represents 5 gamefoot.
 
+	//A test JSON string to use until we pull directly from the server.
+	private string JSONSTRING = @"
+	{
+	""width"": 16,
+	""height"": 2,
+	""depth"": 16,
+	""base_color"": 
+	{
+		""r"": 0.2,
+		""g"": 0.2,
+		""b"": 0.2,
+		""a"": 1.0
+	},
+	""models"": 
+	[
+		{
+		""type"": ""character"",
+		""position"": 
+			{
+				""x"": 1,
+				""y"": 1,
+				""z"": 1
+			},
+			""color"": 
+			{
+				""r"": 0.3,
+				""g"": 1,
+				""b"": 0.4,
+				""a"": 1.0
+			}
+		},
+		{
+		""type"":""npc"",
+		""position"":
+			{
+				""x"": 3,
+				""y"": 1,
+				""z"": 3
+			},
+			""color"":
+			{
+				""r"": 1.0,
+				""g"": 0,
+				""b"": 0,
+				""a"": 1.0
+			}
+		},
+		{
+		""type"":""voxel"",
+		""position"":
+			{
+				""x"": 1,
+				""y"": 1,
+				""z"": 4
+			},
+			""color"":
+			{
+				""r"": 0,
+				""g"": 0,
+				""b"": 255,
+				""a"": 255
+			}
+		},
+		{
+		""type"":""voxel"",
+		""position"":
+			{
+				""x"": 2,
+				""y"": 1,
+				""z"": 4
+			},
+			""color"":
+			{
+				""r"": 0,
+				""g"": 0,
+				""b"": 255,
+				""a"": 255
+			}
+		},
+		{
+		""type"":""voxel"",
+		""position"":
+			{
+				""x"": 1,
+				""y"": 1,
+				""z"": 5
+			},
+			""color"":
+			{
+				""r"": 0,
+				""g"": 0,
+				""b"": 255,
+				""a"": 255
+			}
+		},
+		{
+		""type"":""voxel"",
+		""position"":
+			{
+				""x"": 2,
+				""y"": 1,
+				""z"": 5
+			},
+			""color"":
+			{
+				""r"": 0,
+				""g"": 0,
+				""b"": 255,
+				""a"": 255
+			}
+		}
+	]}";
+
 	//GridItem is serializable to make it JSON friendly.
+	//GridItems represent individual pieces to place on the board.
 	[Serializable]
 	public class GridItem
 	{
+		//All values for this class are default values.
 		public string type = null; //The type of the piece. Could be "character", "tile", or "npc".
 
-		//x, y, z coordinates, and RGBA color floats. For grid, x, y, z are number of tiles in each direction.
-		public int x = 0;
-		public int y = 0;
-		public int z = 0;
-		public float red = 1f;
-		public float green = 1f;
-		public float blue = 1f;
-		public float alpha = 1f;
+		//Vector3 position coordinates, and Vector4 color values.
+		public Vector3 position = new Vector3 (0f, 0f, 0f);
+		public Color color = new Color (1f, 1f, 1f, 1f);
 	}
+
+	//MapItem is serializable to make it JSON friendly.
+	//MapItem contains information about the map and an array of GridItems.
+	[Serializable]
+	public class MapItem
+	{
+		public int width = 1;
+		public int height = 1;
+		public int depth = 1;
+
+		public Color base_color = new Color(1f, 1f, 1f, 1f);
+
+		public GridItem[] models = null;
+	}
+
+
 
 	// Use this for initialization
 	void Start () {
@@ -74,91 +200,47 @@ public class JSONReader : MonoBehaviour {
 			tileLayout.transform.SetParent (mapScaler.transform);
 		}
 
-		//This creates a game object to play around with. Eventually this
-		//will be replaced by retrieving the JSON object from the server.
-		GridItem obj = new GridItem ();
-		obj.type = "character";
-		obj.x = 1;
-		obj.y = 0;
-		obj.z = 1;
-		obj.red = 0.2f;
-		obj.green = 1f;
-		obj.blue = 0.2f;
-		obj.alpha = 1f;
+		MapItem grid = JsonUtility.FromJson<MapItem> (JSONSTRING);
 
-		GridItem grid = new GridItem ();
-		grid.type = "grid";
-		grid.x = 5;
-		grid.y = 2;
-		grid.z = 7;
-		grid.red = 0.2f;
-		grid.green = 0.2f;
-		grid.blue = 0.2f;
-		grid.alpha = 1f;
+		buildMap (grid);
 
-		GridItem tilepiece = new GridItem ();
-		tilepiece.type = "tile";
-		tilepiece.x = 3;
-		tilepiece.y = 0;
-		tilepiece.z = 3;
-		tilepiece.red = 1f;
-		tilepiece.green = 0f;
-		tilepiece.blue = 0f;
-		tilepiece.alpha = 1f;
-
-		GridItem tilepiece1 = new GridItem ();
-		tilepiece1.type = "tile";
-		tilepiece1.x = 4;
-		tilepiece1.y = 0;
-		tilepiece1.z = 3;
-		tilepiece1.red = 1f;
-		tilepiece1.green = 0f;
-		tilepiece1.blue = 0f;
-		tilepiece1.alpha = 1f;
-
-		GridItem tilepiece2 = new GridItem ();
-		tilepiece2.type = "tile";
-		tilepiece2.x = 3;
-		tilepiece2.y = 0;
-		tilepiece2.z = 4;
-		tilepiece2.red = 1f;
-		tilepiece2.green = 0f;
-		tilepiece2.blue = 0f;
-		tilepiece2.alpha = 1f;
-
-		GridItem tilepiece3 = new GridItem ();
-		tilepiece3.type = "tile";
-		tilepiece3.x = 4;
-		tilepiece3.y = 0;
-		tilepiece3.z = 4;
-		tilepiece3.red = 1f;
-		tilepiece3.green = 0f;
-		tilepiece3.blue = 0f;
-		tilepiece3.alpha = 1f;
-
-		GridItem enemy = new GridItem ();
-		enemy.type = "npc";
-		enemy.x = 4;
-		enemy.y = 0;
-		enemy.z = 3;
-		enemy.red = 0f;
-		enemy.green = 0f;
-		enemy.blue = 0f;
-		enemy.alpha = 1f;
-
-		//builds the piece.
-		buildPiece (obj);
-		buildPiece (grid);
-		buildPiece (tilepiece);
-		buildPiece (tilepiece1);
-		buildPiece (tilepiece2);
-		buildPiece (tilepiece3);
-		buildPiece (enemy);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void buildMap (MapItem obj){
+		int row = (int) obj.width;
+		int col = (int) obj.depth;
+		int height = (int) obj.height;
+
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				//Create a grid.
+				Vector3 gridVector = new Vector3 (i, .52f, j);
+				GameObject gridspace = Instantiate (gridPrefab, gridVector, Quaternion.identity);
+				gridspace.transform.SetParent (gridLayout.transform);
+				//Create the plane.
+				Vector3 tilesVector = new Vector3 (i, 0f, j);
+				GameObject tile = Instantiate (tilePrefab, tilesVector, Quaternion.identity);
+				tile.transform.SetParent (tileLayout.transform);
+				fillColor (obj, tile);
+			}
+		}
+		for (int k = 0; k <= height; k++) {
+			if (k % gridHeightGap == 0) {
+				Vector3 gridVector = new Vector3 (getGridSize (row) - 1, k + .52f, getGridSize (col) - 1);
+				GameObject gridOutline = Instantiate (gridPrefab, gridVector, Quaternion.identity);
+				gridOutline.transform.SetParent (gridLayout.transform);
+				gridOutline.transform.localScale = new Vector3 (row, 1, col);
+			}
+		}
+
+		foreach (GridItem model in obj.models) {
+			buildPiece (model);
+		}
 	}
 
 	//Builds a piece based on its type. Places the piece and gives it its color.
@@ -169,43 +251,16 @@ public class JSONReader : MonoBehaviour {
 
 		//Checks the type of the piece. Will be converted to a switch statement to check for all types.
 		switch (piece.type) {
-		case "grid":
-			int row = obj.x;
-			int col = obj.z;
-			int height = obj.y;
 
-			for (int i = 1; i <= row; i++) {
-				for (int j = 1; j <= col; j++) {
-					//Create a grid.
-					Vector3 gridVector = new Vector3 (i, .02f, j);
-					GameObject gridspace = Instantiate (gridPrefab, gridVector, Quaternion.identity);
-					gridspace.transform.SetParent (gridLayout.transform);
-					//Create the plane.
-					Vector3 tilesVector = new Vector3 (i, 0f, j);
-					GameObject tile = Instantiate (tilePrefab, tilesVector, Quaternion.identity);
-					tile.transform.SetParent (tileLayout.transform);
-					fillColor (piece, tile);
-				}
-			}
-			for (int k = 0; k <= height; k++) {
-				if (k % gridHeightGap == 0) {
-					Vector3 gridVector = new Vector3 (getGridSize (row), k, getGridSize (col));
-					GameObject gridOutline = Instantiate (gridPrefab, gridVector, Quaternion.identity);
-					gridOutline.transform.SetParent (gridLayout.transform);
-					gridOutline.transform.localScale = new Vector3 (row, 1, col);
-				}
-			}
-			break;	
-
-		case "tile": 
-			Vector3 tileVector = new Vector3 (piece.x, piece.y + .01f, piece.z); //Tile's position.
+		case "voxel": 
+			Vector3 tileVector = piece.position; //Tile's position.
 			GameObject tilePiece = Instantiate (tilePrefab, tileVector, Quaternion.identity); //Creates a tile prefab.
 			tilePiece.transform.SetParent (tileLayout.transform); //Sets parent to TileLayout GameObject.
 			fillColor (piece, tilePiece); 
 			break;
 
 		case "character":
-			Vector3 charVector = new Vector3 (piece.x, piece.y, piece.z); //Character's position.
+			Vector3 charVector = piece.position; //Character's position.
 			GameObject character = Instantiate (charPrefab, charVector, Quaternion.identity); //Creates a character prefab.
 			character.transform.SetParent (playerLayout.transform); //Sets parent to PlayerLayout GameObject.
 			fillColor (piece, character);
@@ -213,7 +268,7 @@ public class JSONReader : MonoBehaviour {
 		
 
 		case "npc": 
-			Vector3 npcVector = new Vector3 (piece.x, piece.y, piece.z); //Character's position.
+			Vector3 npcVector = piece.position; //Character's position.
 			GameObject npc = Instantiate (npcPrefab, npcVector, Quaternion.identity); //Creates a character prefab.
 			npc.transform.SetParent (npcLayout.transform); //Sets parent to PlayerLayout GameObject.
 			fillColor (piece, npc);
@@ -228,7 +283,19 @@ public class JSONReader : MonoBehaviour {
 		//Foreach loop colors the tiles with the appropriate color.
 		foreach (MeshRenderer mesh in meshes) {
 			Renderer rend = mesh.GetComponent<Renderer> ();
-			Color color = new Color (piece.red, piece.green, piece.blue, piece.alpha);
+			Color color = piece.color;
+			rend.material.color = color;
+		}
+	}
+
+	static void fillColor (MapItem piece, GameObject obj)
+	{
+		MeshRenderer[] meshes = obj.GetComponentsInChildren<MeshRenderer> ();
+		//Gets the child objects of the prefab by finding their MeshRenderer.
+		//Foreach loop colors the tiles with the appropriate color.
+		foreach (MeshRenderer mesh in meshes) {
+			Renderer rend = mesh.GetComponent<Renderer> ();
+			Color color = piece.base_color;
 			rend.material.color = color;
 		}
 	}
