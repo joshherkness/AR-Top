a61870e357d9f5b7ef19d0a5256bbd9b207d2657
Joshua Herkness <joshherkness@gmail.com>
2018-01-23 16:08:16 -0500

    If there are any errors on submit, show them and disable the submit button.

--- vue/src/components/CreateMapModal.vue
+++ vue/src/components/CreateMapModal.vue
@@ -11,87 +11,88 @@
       <div class="card-content">
         <h1 class="title is-4">Create a new map</h1>
         <!-- Map name -->
-        <div class="field">
-          <label class="label">Map name</label>
-          <div class="control">
-            <input
-              name="map name"
-              v-model="name"
-              v-validate="nameValidator"
-              type="text"
-              placeholder="e.g Exandria"
-              :class="{'input': true, 'is-danger': errors.has('map name') }">
-          </div>
-          <span class="help">Think of a cool name for your map</span>
-          <span v-show="errors.has('map name')" class="help is-danger">{{ errors.first('map name') }}</span>
-        </div>
-
-        <!-- Map size -->
-        <div class="field">
-          <label class="label">Map size (optional)</label>
-          <div class="control">
-            <input
-              name="map size"
-              v-model="size"
-              v-validate="sizeValidator"
-              type="text"
-              placeholder="e.g 32"
-              :class="{'input': true, 'is-danger': errors.has('map size') }" >
-          </div>
-          <span class="help">This specifies how large your map will be</span>
-          <span v-show="errors.has('map size')" class="help is-danger">{{ errors.first('map size') }}</span>
-        </div>
-
-        <!-- Map color -->
-        <label class="label">Map color (optional)</label>
-        <div class="field has-addons">
-          <div class="control is-expanded">
-            <input
-              name="map color"
-              v-model="color"
-              v-validate="colorValidator"
-              class="input"
-              type="text"
-              placeholder="e.g #DDDDDD"
-              :class="{'input': true, 'is-danger': errors.has('map color') }">
-            <span class="help">This specifies the color that will be used as the base of your map</span>
-            <span v-show="errors.has('map color')" class="help is-danger">{{ errors.first('map color') }}</span>
-          </div>
-          <!-- Color picker dropdown -->
-          <div class="control">
-            <div class="dropdown is-hoverable is-up is-right is-pulled-right">
-              <div class="dropdown-trigger">
-                <button class="button is-static">
-                  <span class="color-swatch" :style="{'background-color': color}"></span>
-                </button>
-              </div>
-              <div class="dropdown-menu" id="dropdown-menu4" role="menu">
-                <sketch-picker v-model="colorData"></sketch-picker>
-              </div>
+        <form @submit.prevent="validateBeforeSubmit">
+          <div class="field">
+            <label class="label">Map name</label>
+            <div class="control">
+              <input
+                name="map name"
+                v-model="name"
+                v-validate="nameValidator"
+                type="text"
+                placeholder="e.g Exandria"
+                :class="{'input': true, 'is-danger': errors.has('map name') }">
             </div>
-
+            <span class="help">Think of a cool name for your map</span>
+            <span v-show="errors.has('map name')" class="help is-danger">{{ errors.first('map name') }}</span>
           </div>
-        </div>
 
-        <!-- Other settings -->
-        <div class="field">
-          <label class="label">Other settings</label>
-          <div class="control">
-            <label class="checkbox">
-              <input v-model="public" class="checkbox" type="checkbox">
-              Public
-            </label>
+          <!-- Map size -->
+          <div class="field">
+            <label class="label">Map size (optional)</label>
+            <div class="control">
+              <input
+                name="map size"
+                v-model="size"
+                v-validate="sizeValidator"
+                type="text"
+                placeholder="e.g 32"
+                :class="{'input': true, 'is-danger': errors.has('map size') }" >
+            </div>
+            <span class="help">This specifies how large your map will be</span>
+            <span v-show="errors.has('map size')" class="help is-danger">{{ errors.first('map size') }}</span>
           </div>
-        </div>
 
-        <!-- Buttons -->
-        <div class="field is-grouped is-grouped-right">
-          <p class="control">
-            <button  v-on:click="submit" class="button is-link">Submit</button>
-            <button  v-on:click="cancel" class="button is-white">Cancel</button>
-          </p>
-        </div>
+          <!-- Map color -->
+          <label class="label">Map color (optional)</label>
+          <div class="field has-addons">
+            <div class="control is-expanded">
+              <input
+                name="map color"
+                v-model="color"
+                v-validate="colorValidator"
+                class="input"
+                type="text"
+                placeholder="e.g #DDDDDD"
+                :class="{'input': true, 'is-danger': errors.has('map color') }">
+              <span class="help">This specifies the color that will be used as the base of your map</span>
+              <span v-show="errors.has('map color')" class="help is-danger">{{ errors.first('map color') }}</span>
+            </div>
+            <!-- Color picker dropdown -->
+            <div class="control">
+              <div class="dropdown is-hoverable is-up is-right is-pulled-right">
+                <div class="dropdown-trigger">
+                  <button class="button is-static">
+                    <span class="color-swatch" :style="{'background-color': color}"></span>
+                  </button>
+                </div>
+                <div class="dropdown-menu" id="dropdown-menu4" role="menu">
+                  <sketch-picker v-model="colorData"></sketch-picker>
+                </div>
+              </div>
 
+            </div>
+          </div>
+
+          <!-- Other settings -->
+          <div class="field">
+            <label class="label">Other settings</label>
+            <div class="control">
+              <label class="checkbox">
+                <input v-model="public" class="checkbox" type="checkbox">
+                Public
+              </label>
+            </div>
+          </div>
+
+          <!-- Buttons -->
+          <div class="field is-grouped is-grouped-right">
+            <p class="control">
+              <button v-on:click="submit" class="button is-link" type="submit" :disabled="errors.any()">Submit</button>
+              <button v-on:click="cancel" class="button is-white">Cancel</button>
+            </p>
+          </div>
+        </form>
       </div>
     </div>
   </modal>
@@ -152,6 +153,9 @@ export default {
     }
   },
   methods: {
+    validateBeforeSubmit: function () {
+      this.$validator.validateAll()
+    },
     submit: function () {
       // Ensure there are no errors in any of the fields
       if (this.errors.any()) {
