13257f3e578afeb966a0138ca393710718271110
KaJuan Johnson <kdjohnson@users.noreply.github.com>
2018-01-26 07:46:30 -0500

    Persist auth (#91)
    * Persist auth with Cookie for 1 day #58
    
    * Remove console log, state deconstruct #58

--- vue/src/store/store.js
+++ vue/src/store/store.js
@@ -1,11 +1,23 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import user from './models/user.js'
+import * as Cookies from 'js-cookie'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     user
-  }
+  },
+  plugins: [
+    createPersistedState({
+      storage: {
+        getItem: key => Cookies.get(key),
+        // expires after 1 day
+        setItem: (key, value) => Cookies.set(key, value, { expires: 1 }),
+        removeItem: key => Cookies.remove(key)
+      }
+    })
+  ]
 })
