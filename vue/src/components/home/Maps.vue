dcc015442b5285ea68e8b8a4402acf4519604ebb
Joshua Herkness <joshherkness@gmail.com>
2018-01-25 22:03:19 -0500

    Add delete map modal (UI) which still needs to be linked with API
    closes #77

--- vue/src/components/home/Maps.vue
+++ vue/src/components/home/Maps.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
     <create-map-modal/>
+    <delete-map-modal/>
     <div>
-      <button class="button is-light is-pulled-right" @click="$modal.show('create-map-modal')">
+      <button class="button is-light is-pulled-right" @click="$modal.show('create-map-modal', {test: 'josh'})">
         <span class="icon">
           <i class="fa fa-plus"></i>
         </span>
         <span>Create a map</span>
+      </button>
+      <button class="button is-light is-pulled-right" @click="$modal.show('delete-map-modal', {name: 'Exandria'})">
+        <span class="icon">
+          <i class="fa fa-trash"></i>
+        </span>
+        <span>Delete a map</span>
       </button>
     </div>
   </div>
@@ -14,10 +21,12 @@
 
 <script>
 import CreateMapModal from '../CreateMapModal'
+import DeleteMapModal from '../DeleteMapModal'
 export default {
   name: 'Maps',
   components: {
-    CreateMapModal
+    CreateMapModal,
+    DeleteMapModal
   }
 }
 </script>
