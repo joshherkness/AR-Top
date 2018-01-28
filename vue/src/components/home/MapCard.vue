501a60045bf73b9e71f417a55aad3791178cfa6a
KaJuan Johnson <kdjohnson23@oakland.edu>
2018-01-28 16:34:41 -0500

    Add more margin to cards, Change delete default button color #79

--- vue/src/components/home/MapCard.vue
+++ vue/src/components/home/MapCard.vue
@@ -6,7 +6,7 @@
     </div>
     <div class="buttons is-pulled-right">
       <span class="button is-info" @click="$modal.show('edit-map-modal', {name: name, width: width, depth: depth})">Edit</span>
-      <span class="button is-danger" @click="$modal.show('delete-map-modal', {name: name})">Delete</span>
+      <span class="button" @mouseover="mouseOver" @mouseleave="mouseLeave" v-bind:class="[isHovered ? 'is-danger' : 'is-light']" @click="$modal.show('delete-map-modal', {name: name})">Delete</span>
     </div>
   </div>
 </template>
@@ -14,15 +14,28 @@
 <script>
 export default {
   name: 'MapCard',
-  props: ['name', 'width', 'depth']
+  props: ['name', 'width', 'depth'],
+  data: function () {
+    return {
+      isHovered: false
+    }
+  },
+  methods: {
+    mouseOver: function () {
+      this.isHovered = true
+    },
+    mouseLeave: function () {
+      this.isHovered = false
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 @import '~bulma/bulma.sass';
 .box {
-  flex: 0 0 calc((100% / 3) - 1em);
+  flex: 0 0 calc((100% / 3) - 4em);
   height: 100%;
-  margin: 0.5em;
+  margin: 0 2em 1em;
 }
 </style>
