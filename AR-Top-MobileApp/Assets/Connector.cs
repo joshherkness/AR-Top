d4e85291da49d5d97bb397542b2c3ee79b2ce596
goeteeks <nferman@oakland.edu>
2018-01-29 16:03:10 -0500

    Added a socket event for RoomNotFound
    In the event the room code submitted by a user isn't found, sends a
    signal back to Unity to display an error message.

--- AR-Top-MobileApp/Assets/Connector.cs
+++ AR-Top-MobileApp/Assets/Connector.cs
@@ -6,19 +6,24 @@ using SocketIO;
 public class Connector : MonoBehaviour {
 
 	SocketIOComponent socket;
+	RoomManager room;
 
 	// Use this for initialization
 	void Start () {
 		socket = GameObject.Find ("SocketIO").GetComponent <SocketIOComponent> ();
+		room = FindObjectOfType<RoomManager> ();
+
 		//socket.On("connect", connection);
 		socket.On ("update", TestBoop);
+		//socket.Emit ("RoomNotFound");
+		socket.On ("RoomNotFound", roomNotFound);
 
 		StartCoroutine (BoopTime ());
 	}
 
 	// Update is called once per frame
 	void Update () {
-
+		
 	}
 
 	private IEnumerator BoopTime(){
@@ -28,13 +33,20 @@ public class Connector : MonoBehaviour {
 		data["email"] = "some@email.com";
 		data["pass"] = "1234";
 		socket.Emit ("update", new JSONObject (data));
+		//socket.Emit ("RoomNotFound", new JSONObject (data));
 	}
 
 	public void connection(SocketIOEvent e){
-		socket.Emit ("connect", new JSONObject (e.data));
+		socket.Emit ("RoomNotFound", new JSONObject (e.data));
 	}
 
 	public void TestBoop(SocketIOEvent e){
+		Debug.Log ("Update Called"); 
 		Debug.Log(string.Format ("[name: {0}, data: {1}]", e.name, e.data));
+	}
+
+	public void roomNotFound(SocketIOEvent e){
+		Debug.Log ("Signal of 'Room Not Found' received."); 
+		room.roomNotFound ();
 	}
 }
--- server/server.py
+++ server/server.py
@@ -150,7 +150,7 @@ def authenticate():
 socketio = SocketIO(app)
 
 @socketio.on('connect')
-def handle_event():
+def handle_connect():
 	print("a user connected")
 
 @socketio.on('disconnect')
@@ -161,6 +161,11 @@ def handle_disconnect():
 def handle_update(json):
 	print('received json: ' + str(json))
 	emit('update', json)
+
+#TODO: Send this in the event the submitted room code isn't found
+def handle_RoomNotFound(json):
+	emit('RoomNotFound', json)
+	print ('Room Not Found!')
 
 #=====================================================
 # Main
