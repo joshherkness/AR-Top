9d56f6cdc7c29d364a9c1ccb052c43b128254919
Joshua Herkness <joshherkness@gmail.com>
2018-02-04 17:13:09 -0500

    Move my library section header into a bulma level element

--- vue/src/components/home/Maps.vue
+++ vue/src/components/home/Maps.vue
@@ -1,20 +1,28 @@
 <template>
   <div class="section">
     <div class="container">
-      <div class="title is-5">
-        <span>My Library</span>
-        <button 
-          class="button is-light is-pulled-right" 
-          @click="$modal.show('create-map-modal')">
-          <span class="icon">
-            <i class="fa fa-plus"></i>
-          </span>
-          <span>Create a map</span>
-        </button>
+      <div class="level">
+        <div class="level-left">
+          <div class="level-item">
+            <span class="title is-5">My Library</span>
+          </div>
+        </div>
+        <div class="level-right">
+          <div class="level-item">
+            <button 
+              class="button is-light is-pulled-right" 
+              @click="$modal.show('create-map-modal')">
+              <span class="icon">
+                <i class="fa fa-plus"></i>
+              </span>
+              <span>Create a map</span>
+          </button>
+          </div>
+        </div>
       </div>
+      <hr>
+      <map-list />
     </div>
-    <hr>
-    <map-list />
   </div>
 </template>
 
