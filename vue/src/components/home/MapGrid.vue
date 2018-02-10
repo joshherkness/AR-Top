c21b319dfa560f3f243f335bccef5760f2515fca
KaJuan Johnson <kdjohnson@users.noreply.github.com>
2018-02-09 20:12:05 -0500

    Add list view for my library #160 (#161)
    * Add list view for my library #160
    
    * Created separate components for map list and map grid. I've also implemented vuex for library layout and renamed some files.

---
+++ vue/src/components/home/MapGrid.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="map-cards">
+    <map-card v-for="map in maps"
+      :key="map._id.$oid"
+      v-bind:name="map.name"
+      v-bind:oid="map._id.$oid"
+      v-bind:color="map.color"
+      v-bind:depth="map.depth"
+      v-bind:width="map.width"/>
+  </div>
+</template>
+
+<script>
+import MapCard from './MapCard'
+
+export default {
+  name: 'MapGrid',
+  props: ['maps'],
+  components: {
+    MapCard
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~bulma/bulma.sass';
+.map-cards {
+  width: 100%;
+  display: flex;
+  flex-flow: row wrap;
+}
+@media (max-width: 32em) {
+  .map-cards {
+    display: flex;
+    flex-direction: column;
+  }
+}
+</style>
--- vue/src/store/mutation-types.js
+++ vue/src/store/mutation-types.js
@@ -6,3 +6,5 @@ export const ADD_MAP = 'ADD_MAP'
 export const REMOVE_MAP = 'REMOVE_MAP'
 export const REMOVE_ALL_MAPS = 'REMOVE_ALL_MAPS'
 export const UPDATE_MAP = 'UPDATE_MAP'
+
+export const SET_LAYOUT = 'SET_LAYOUT'
