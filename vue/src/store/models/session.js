09e7f391d1df2486ccb121d2464e5fe2a7cbcb31
Joshua Herkness <joshherkness@gmail.com>
2018-02-11 13:59:57 -0500

    Fix minor syntax errors for missing commas

--- vue/src/store/models/session.js
+++ vue/src/store/models/session.js
@@ -17,7 +17,7 @@ const actions = {
   // eslint-disable-next-line
   updateSession({ commit }, session) {
     commit(types.UPDATE_SESSION, session)
-  }
+  },
 
   // eslint-disable-next-line
   removeSession({ commit }) {
@@ -34,7 +34,7 @@ const mutations = {
   // eslint-disable-next-line
   [types.UPDATE_SESSION](state, session) {
     state.session = session
-  }
+  },
 
   // eslint-disable-next-line
   [types.REMOVE_SESSION](state) {
