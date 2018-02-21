6d1818ca4959fee28d7aa33815ec4465bc5cb3cf
KaJuan Johnson <kdjohnson23@oakland.edu>
2018-02-21 12:31:16 -0500

    Add scrolling to list and grid view #152

--- vue/src/components/MapList.vue
+++ vue/src/components/MapList.vue
@@ -1,26 +1,41 @@
 <template>
-  <table class="table is-fullwidth">
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Size</th>
-        <th>Color</th>
+  <div>
+    <table class="table is-fullwidth">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Size</th>
+          <th>Color</th>
 
-        <!-- Blank columns used for right aligned controls -->
-        <th></th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      <map-row v-for="map in maps"
-      :key="map._id.$oid"
-      v-bind:name="map.name"
-      v-bind:oid="map._id.$oid"
-      v-bind:color="map.color"
-      v-bind:depth="map.depth"
-      v-bind:width="map.width"/>
-    </tbody>
-  </table>
+          <!-- Blank columns used for right aligned controls -->
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+
+      <!-- For header alignment -->
+      <tbody class="empty">
+        <td class="is-narrow"></td>
+        <td class="is-narrow"></td>
+        <td class="is-narrow"></td>
+        <td></td>
+        <td class="is-narrow"></td>
+      </tbody>
+    </table>
+    <div class="table-content">
+      <table class="table is-fullwidth">
+        <tbody>
+          <map-row v-for="map in maps"
+          :key="map._id.$oid"
+          v-bind:name="map.name"
+          v-bind:oid="map._id.$oid"
+          v-bind:color="map.color"
+          v-bind:depth="map.depth"
+          v-bind:width="map.width"/>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -35,3 +50,13 @@ export default {
 }
 </script>
 
+<style lang="scss" scoped>
+.table-content {
+  max-height: 465px;
+  overflow: auto; 
+}
+
+.empty {
+  visibility: collapse;
+}
+</style>
