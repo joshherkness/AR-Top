0c4f4a92f74461f20f4dbdae64296f162b92710a
goeteeks <nferman@oakland.edu>
2018-02-23 02:08:22 -0500

    Initial Panel and Added Text-Mesh Pro
    - Added Text Mesh Pro for nice, crisp looking fonts.
    - Created a Panel with Dropdowns for 2 settings (Anti-Aliasing and Grid
    Outlines).

---
+++ AR-Top-MobileApp/Assets/UserSettings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserSettings : MonoBehaviour {
+
+	public RectTransform panel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
