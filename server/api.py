09b37f97344e250f05a2586f3da6942de4946351
Johnathon Robertson <johnathon.j.robertson@gmail.com>
2018-03-05 13:20:20 -0500

    69 read session by user id endpoint (#212)
    * added read session by user id endpoint
    
    * tests are unable to send requests properly
    
    * added tests for read_session_user_id endpoint

--- server/api.py
+++ server/api.py
@@ -1,4 +1,6 @@
+import sys
 import traceback
+from json import loads
 from datetime import datetime
 
 from flask import current_app, jsonify, request
@@ -158,6 +160,7 @@ class Api():
             email = claims["email"]
             user = User.objects(email=email).first()
             map = request.json['map']
+            map = loads(map)
         except Exception as e:
             if not current_app.testing:
                 current_app.logger.error(str(e))
@@ -365,3 +368,19 @@ class Api():
             return internal_error()
 
         return jsonify(success="Successfully read session", session=remote_copy), 200, json_tag
+
+    def read_session_user_id(claims, token_user):
+        """ Read a session with given user_id """
+        try:
+            session_entity = Session.objects(user_id=token_user.id).first()
+        except (StopIteration, DoesNotExist) as e:
+            return jsonify(error="Session does not exist"), 404, json_tag
+        except Exception as e:
+            current_app.logger.error(str(e))
+            return internal_error()
+
+        if session_entity == None:
+            return jsonify(error="Session does not exist"), 404, json_tag
+
+        return jsonify(success="Successfully read session", session=session_entity), 200, json_tag
+
