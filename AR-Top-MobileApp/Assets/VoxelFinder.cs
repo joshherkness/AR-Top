918985743ece99b0cbe4df0a1eabda994775e1e8
jjrobertson14 <johnathon.j.robertson@gmail.com>
2018-03-19 19:39:05 -0400

    added text to show where reticule is pointed at

--- AR-Top-MobileApp/Assets/VoxelFinder.cs
+++ AR-Top-MobileApp/Assets/VoxelFinder.cs
@@ -1,21 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class VoxelFinder : MonoBehaviour
 {
 	private GameObject camera;
+	private TextMeshProUGUI displayTextBox;
+	private string coordinatesText;
+	private GameObject prevHitObject;
 
 	// Use this for initialization
 	void Start () {
 		camera = GameObject.FindGameObjectWithTag("MainCamera");
+		displayTextBox = GameObject.FindGameObjectWithTag("CoordinatesDisplay").GetComponent<TextMeshProUGUI>();
 		gameObject.transform.SetParent(camera.transform);
+		coordinatesText = "(0,0,0)\n(x,y,z)";
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		GameObject hitObject = findVoxel();
-		Debug.Log(hitObject.transform.position.ToString());
+		if (hitObject != prevHitObject)
+		{
+			Vector3 hitObjectPos = hitObject.transform.position * 10;
+			coordinatesText = "(" + Math.Round(hitObjectPos.x) + ',';
+			coordinatesText += Math.Round(hitObjectPos.z) + ",";
+			coordinatesText += Math.Round(hitObjectPos.y) + ")";
+			displayTextBox.text = coordinatesText;
+		}
+		prevHitObject = hitObject;
 	}
 
 	public GameObject findVoxel()
