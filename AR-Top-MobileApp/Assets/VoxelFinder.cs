6b548fe4e4d9b1d773b8a08a062c1a2923a2ed33
jjrobertson14 <johnathon.j.robertson@gmail.com>
2018-03-19 18:50:33 -0400

    Added raycast code to VoxelFinder.cs, reticule, and put VoxelFinder on an object with the same name

---
+++ AR-Top-MobileApp/Assets/VoxelFinder.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoxelFinder : MonoBehaviour
+{
+	private GameObject camera;
+
+	// Use this for initialization
+	void Start () {
+		camera = GameObject.FindGameObjectWithTag("MainCamera");
+		gameObject.transform.SetParent(camera.transform);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject hitObject = findVoxel();
+		Debug.Log(hitObject.transform.position.ToString());
+	}
+
+	public GameObject findVoxel()
+	{
+		//cast ray from camera in forward camera direction
+		var finderRay = new Ray(camera.transform.position, camera.transform.forward);
+		var resultHit = new RaycastHit();
+		Physics.Raycast(finderRay, out resultHit);
+		//whatever ray hits, get info
+		return resultHit.collider.gameObject;
+	}
+}
