c44b9ff63b69fcd84935bf8d1e0982bf60d21547
Joshua Herkness <joshherkness@gmail.com>
2018-04-05 16:28:57 -0400

    Update steps listed on home page, as well as adding logo to the hero.

--- vue/src/components/Home.vue
+++ vue/src/components/Home.vue
@@ -3,9 +3,15 @@
     <section class="hero is-large is-link">
       <div class="hero-body">
         <div class="container">
-          <h1 class="title has-text-centered">
-            AR Top
-          </h1>
+          <div style="display: grid;
+              grid-template-columns: 1fr;
+              grid-gap: 0px 0px;
+              justify-items: center;">
+            <figure class="image" style="margin-top: 1rem; width: 25%">
+              <img src="@/assets/artop-logo-light.svg">
+            </figure>
+          </div>
+          <br>
           <h2 class="subtitle has-text-centered">
             Let us define our world.
           </h2>
@@ -24,10 +30,12 @@
         <div class="columns is-vcentered is-centered">
           <div class="column is-half">
             <h1 class="title is-4">
-              <span class="has-text-info">Step 1:</span> Sign up
+              <span class="has-text-info icon">
+                <i class="mdi mdi-numeric-1-box-outline"></i>
+              </span> Register
             </h1>
             <router-link class="button is-link" :to="{ name : 'register'}">
-              Sign up
+              Register
             </router-link>
           </div>
         </div>
@@ -39,12 +47,14 @@
         <div class="columns is-vcentered is-centered">
           <div class="column is-half">
             <h1 class="title is-4">
-              <span class="has-text-info">Step 2:</span> Create a map
+              <span class="has-text-info icon">
+                <i class="mdi mdi-numeric-2-box-outline"></i>
+              </span> Create a map
             </h1>
             <p>
               Once you've registered for an account, you can start creating
-              maps. This can be done by navigating to your <i><b>Library</b></i>
-              and clicking the <i><b>Create a map</b></i> button in the top right.
+              maps. This can be done by navigating to <i><b>My Library</b></i>
+              and clicking <i><b>Create a map</b></i> in the top right.
             </p>
           </div>
         </div>
@@ -56,37 +66,41 @@
         <div class="columns is-vcentered is-centered">
           <div class="column is-half">
             <h1 class="title is-4">
-              <span class="has-text-info">Step 3:</span> Create a session
-            </h1>
-            <p>
-              After your map is created, and you are ready to show it off, create a
-              session.
-            </p>
-          </div>
-        </div>
-      </div>
-    </section>
-    <hr>
-    <section class="section">
-      <div class="container">
-        <div class="columns is-vcentered is-centered">
-          <div class="column is-half">
-            <h1 class="title is-4">
-              <span class="has-text-info">Step 4:</span> Print the target image
+             <span class="has-text-info icon">
+                <i class="mdi mdi-numeric-3-box-outline"></i>
+              </span> Open a session
             </h1>
             <div class="content">
               <p>
-                This image will be used by the mobile application to display an
-                augmented render of your map, that can be seen by anyone in your
-                session.
+                Once you are ready to have others view your map, you can click
+                <i><b>Open session</b></i> in the navigation bar revealing the 
+                <i><b>Session manager</b></i> at the bottom of your screen, where
+                you can find your invitation code.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <hr>
+    <section class="section">
+      <div class="container">
+        <div class="columns is-vcentered is-centered">
+          <div class="column is-half">
+            <h1 class="title is-4">
+              <span class="has-text-info icon">
+                <i class="mdi mdi-numeric-4-box-outline"></i>
+              </span> Print our logo
+            </h1>
+            <div class="content">
+              <p>
+                This image is used as a target on which the mobile application will render 
+                your map. You can print this image using the button below.
               </p>
             </div>
             <button class="button is-link" v-on:click="openImageTarget">
               Print image target
             </button>
-            <figure class="image is-128x128" style="margin: 1rem;">
-              <img src="@/assets/image-target.jpg">
-            </figure>
           </div>
         </div>
       </div>
@@ -97,12 +111,20 @@
         <div class="columns is-vcentered is-centered">
           <div class="column is-half">
             <h1 class="title is-4">
-              <span class="has-text-info">Step 5:</span> Download the mobile app
+              <span class="has-text-info icon">
+                <i class="mdi mdi-numeric-5-box-outline"></i>
+              </span> Gather your party
             </h1>
-            <p>
-              Your friends can now download the mobile application, and join your
-              session using the invitation code that was generated for you.
-            </p>
+            <div class="content">
+              <p>
+                Have the members of your party download our mobile application.
+              </p>
+              <p>
+                They can connect to your session by entering your invitation 
+                code. Once connected, they can simply focus their cameras on the 
+                printed logo to view your map.
+              </p>
+            </div>
           </div>
         </div>
       </div>
@@ -110,7 +132,7 @@
   </div>
 </template>
 <script>
-import imageTarget from '@/assets/image-target.jpg'
+import imageTarget from '@/assets/target-image.png'
 
 export default {
   name: 'Home',
